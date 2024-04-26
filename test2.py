5cbc979cd88ed4841d8da9f22d61e63054c07a34
MAGIC祺 <167555285+majiaqi02@users.noreply.github.com>
2024-04-26 09:08:47 +0800

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,18 +1,25 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n =len(nums)
-        nums.sort()
+def twoSum(nums, target):
+    # 创建一个字典来存储数组元素和它们的索引
+    num_dict = {}
 
-        b = n - 1
-        for a in range(n):
-            if a>0 and nums[a]== nums[a-1]:
-                continue
+    # 遍历数组
+    for i, num in enumerate(nums):
+        # 计算目标值与当前值的差
+        complement = target - num
 
-            while a<b and nums[a]  + nums[b] > target :
-                b -= 1
-            if a==b:
-                break
-                if nums[a] + nums[b] == target:
-                    return [nums[a], nums[b]]
+        # 检查差是否已经在字典中
+        if complement in num_dict:
+            # 如果在，返回结果
+            return [num_dict[complement], i]
 
-        return[]
+            # 如果不在，将当前值及其索引添加到字典中
+        num_dict[num] = i
+
+        # 如果没有找到符合条件的两个数，返回空列表（或者抛出异常）
+    return []
+
+
+# 测试
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))  # 输出应该是 [0, 1]，因为 nums[0] + nums[1] = 2 + 7 = 9
