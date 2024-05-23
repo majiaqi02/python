11fc496a03f4a8bc8c79bd6668668722103a8b42
MAGIC祺 <167555285+majiaqi02@users.noreply.github.com>
2024-05-23 19:21:57 +0800

    Add files via upload

---
+++ 三数最大乘积.py
@@ -0,0 +1,10 @@
+from typing import List
+def maxadd(nums: List[int]) -> int:
+    if len(nums) < 3:
+        print("输入字符数量过少，请输入至少三个数字")
+    else:
+        nums.sort()
+        print(max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1]))
+
+nums = [1, 2, 3]
+maxadd(nums)
