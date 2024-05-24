f350fb22b9d326160e7e9015caf73e533fa17141
MAGIC祺 <167555285+majiaqi02@users.noreply.github.com>
2024-05-24 10:40:33 +0800

    lambda匿名函数.py

--- lambda匿名函数.py
+++ lambda匿名函数.py
@@ -4,11 +4,14 @@ def test_func(compute):
 
 def compute(x,y):
     return x + y
-
+# 使用命名函数 compute 作为参数  
 test_func(compute)
 
 def test_func(compute):
     result = compute(1, 2)
     print(result)
-
+# 使用 lambda 表达式作为参数 
 test_func(lambda x, y: x + y)
+
+
+
