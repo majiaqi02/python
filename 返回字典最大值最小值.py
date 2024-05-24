acfee117d4a2b668d59b9a8653e6cfe5c3aeb65d
MAGIC祺 <167555285+majiaqi02@users.noreply.github.com>
2024-05-24 08:49:58 +0800

    Add files via upload

---
+++ 返回字典最大值最小值.py
@@ -0,0 +1,11 @@
+def func(*args):
+    the_max=args[0]
+    the_min = args[0]
+    for i in args:
+        if i>the_max:
+            the_max=i
+        elif i<the_min:
+            the_min=i
+    return{'max':the_max,'min':the_min}
+res=func(1,20,3,40,5)
+print(res)
